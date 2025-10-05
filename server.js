61a5b9bd816bd404ed267fa006a865b6519d9d93
FarhanDevop <farhanashraf.devops@outlook.com>
2025-10-05 21:54:19 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('My node ap is updated');
+  res.send('Welcome to My project');
 });
 
 app.listen(PORT, HOST, () => {
